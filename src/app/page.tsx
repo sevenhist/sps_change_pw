50321444acb77ccc701f47b36a7e9d1391a12a78
Rudnenko Dmytro <sevenhist@gmail.com>
2025-05-06 11:41:10 +0200

    verbindung einstellung

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           jsonrpc: "2.0",
           method: "PlcProgram.Read",
           params: {
-            var: '"somedata_1".counter',
+            var: '"somedata".counter',
           },
           id: 2,
         };
