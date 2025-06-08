167fb33ad2d9183ade55e5350d0e5379f39ea8a4
Rudnenko Dmytro <sevenhist@gmail.com>
2025-06-08 09:00:18 +0200

    zahl überall gerundet ist

--- src/app/Temp.tsx
+++ src/app/Temp.tsx
@@ -9,6 +9,12 @@ interface TempChartProps {
 }
 
 const TempChart: React.FC<TempChartProps> = ({ tempData, oilData, rpmData }) => {
+  Highcharts.setOptions({
+    time: {
+      timezone: 'Europe/Vienna'
+    }
+  });
+
   const options: Highcharts.Options = {
     chart: {
       type: "spline",
@@ -30,12 +36,22 @@ const TempChart: React.FC<TempChartProps> = ({ tempData, oilData, rpmData }) => 
         title: { text: "Temperatur (°C)" },
         min: 0,
         max: 120,
+        labels: { // subscribing on y axis
+          formatter: function () {
+            return Math.round(this.value as number).toString();
+          }
+        }
       },
       {
         title: { text: "RPM" },
         min: 0,
         max: 5000,
         opposite: true, // rpm scale on the right side
+        labels: { // subscribing on y axis
+          formatter: function () {
+            return Math.round(this.value as number).toString();
+          }
+        }
       },
     ],
     series: [
@@ -64,6 +80,13 @@ const TempChart: React.FC<TempChartProps> = ({ tempData, oilData, rpmData }) => 
     tooltip: {
       xDateFormat: "%H:%M:%S", // datum zeigt richtig
       shared: true, // um alle elemente gleichzeitig zeigen
+      formatter: function () {
+        let s = `<b>${Highcharts.dateFormat('%H:%M:%S', this.x)}</b><br/>`; // fürs datum
+        this.points?.forEach(point => { // points ist ein Array von Punkten 
+          s += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}: <b>${Math.round(point.y as number)}</b><br/>`;
+        });
+        return s;
+      }
     },
     credits: { enabled: false }, // damit keine beschreibung wird sogennante: Highcharts.com
   };
